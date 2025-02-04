16e30721416ba05c0057af54d0f26f837e21ec78
Oğuzhan Kurum <oguzhankurumdev@gmail.com>
2025-02-04 17:27:06 +0300

    feat: add selector for total quantity of items in cart

--- src/redux/features/cart/cartSelectors.ts
+++ src/redux/features/cart/cartSelectors.ts
@@ -4,3 +4,6 @@ export const selectCartItems = (state: RootState) => state.cart.items;
 
 export const selectCartTotal = (state: RootState) =>
   state.cart.items.reduce((total, item) => total + item.price * item.quantity, 0);
+
+export const selectCartTotalQuantity = (state: RootState) =>
+  state.cart.items.reduce((total, item) => total + item.quantity, 0);
