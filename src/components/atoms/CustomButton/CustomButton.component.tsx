42bd6171267ec0be1f91aea0c4d9fda379a52ba9
Oğuzhan Kurum <oguzhankurumdev@gmail.com>
2025-02-12 11:37:05 +0300

    feat: integrate FlashList for improved performance in Products component and optimize CustomButton with memoization

--- src/components/atoms/CustomButton/CustomButton.component.tsx
+++ src/components/atoms/CustomButton/CustomButton.component.tsx
@@ -1,20 +1,23 @@
+import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
 import styles from './CustomButton.style';
 import { CustomButtonProps } from './CustomButton.types';
 
-const CustomButton: React.FC<CustomButtonProps> = ({ title, onPress, overrideContainerStyle }) => {
-  const { container, text } = styles;
+const CustomButton: React.FC<CustomButtonProps> = React.memo(
+  ({ title, onPress, overrideContainerStyle }) => {
+    const { container, text } = styles;
 
-  return (
-    <TouchableOpacity
-      style={[container, overrideContainerStyle]}
-      onPress={onPress}
-      activeOpacity={0.5}
-    >
-      <Text style={text}>{title}</Text>
-    </TouchableOpacity>
-  );
-};
+    return (
+      <TouchableOpacity
+        style={[container, overrideContainerStyle]}
+        onPress={onPress}
+        activeOpacity={0.5}
+      >
+        <Text style={text}>{title}</Text>
+      </TouchableOpacity>
+    );
+  }
+);
 
 export default CustomButton;
