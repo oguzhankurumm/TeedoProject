da5c016202bcad0031381be9b5ed3371f00e4766
Oğuzhan Kurum <oguzhankurumdev@gmail.com>
2025-03-11 21:51:16 +0300

    feat: add analytics tools

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+
+const defaultConfig = getSentryExpoConfig(__dirname);
 defaultConfig.resolver.sourceExts.push('cjs');
 
 module.exports = defaultConfig;
